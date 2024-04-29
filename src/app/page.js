e77ea6b1f25f4e3e98508da2778745e4ef1205a1
mohametalmeari <mohamet.almeari@gmail.com>
2024-04-29 15:52:09 +0300

    Implement parallax scroll animation

--- src/app/page.js
+++ src/app/page.js
@@ -3,33 +3,59 @@
 import styles from "./page.module.scss";
 import Image from "next/image";
 
+import { useTransform, useScroll, motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
+import { useDimension } from "@/useDimension";
+
 const images = Array(12)
   .fill()
   .map((_, i) => `${i + 1}.jpg`);
 
 export default function Home() {
+  const container = useRef();
+  const { height } = useDimension();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end start"],
+  });
+
+  const y1 = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.spacer} />
-      <div className={styles.gallery}>
-        <Column images={[images[0], images[1], images[2]]} />
-        <Column images={[images[3], images[4], images[5]]} />
-        <Column images={[images[6], images[7], images[8]]} />
-        <Column images={[images[9], images[10], images[11]]} />
+      <div ref={container} className={styles.gallery}>
+        <Column images={[images[0], images[1], images[2]]} y={y1} />
+        <Column images={[images[3], images[4], images[5]]} y={y2} />
+        <Column images={[images[6], images[7], images[8]]} y={y3} />
+        <Column images={[images[9], images[10], images[11]]} y={y4} />
       </div>
       <div className={styles.spacer} />
     </main>
   );
 }
 
-const Column = ({ images }) => {
+const Column = ({ images, y = 0 }) => {
   return (
-    <div className={styles.column}>
+    <motion.div style={{ y }} className={styles.column}>
       {images.map((src, index) => (
         <div key={index} className={styles.imageContainer}>
           <Image src={`/images/${src}`} fill alt="image" />
         </div>
       ))}
-    </div>
+    </motion.div>
   );
 };
---
+++ src/useDimension.js
@@ -0,0 +1,17 @@
+const { useState, useEffect } = require("react");
+
+export const useDimension = () => {
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+  const updateDimension = () => {
+    const { innerWidth, innerHeight } = window;
+    setDimension({ width: innerWidth, height: innerHeight });
+  };
+  useEffect(() => {
+    updateDimension();
+    window.addEventListener("resize", updateDimension);
+    return () => {
+      window.removeEventListener("resize", updateDimension);
+    };
+  }, []);
+  return dimension;
+};
